a51ebc832ae2e686efb31793c94ccd28494f6c30
Jun <chchdelm3@icloud.com>
2022-03-16 13:16:41 +0900

    function of shortcut keys

--- MyNotepad/MyNotepad/FrmNote.cs
+++ MyNotepad/MyNotepad/FrmNote.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,39 @@ namespace MyNotepad
                 tBoxMain.WordWrap = true;
                 자동줄바꿈ToolStripMenuItem.Checked = true;
             }
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            sd.Title = "다른 이름으로 저장";
+            sd.ShowDialog();
+
+            if (sd.FileName != "")
+            {
+                File.WriteAllText(sd.FileName, tBoxMain.Text);
+            }
+
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            od.Title = "파일 열기";
+            od.ShowDialog();
+            tBoxMain.Text = File.ReadAllText(od.FileName);
+        }
+
+        private void 새로만들기NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tBoxMain.Text = "";
+        }
+
+        private void 피드백보내ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
